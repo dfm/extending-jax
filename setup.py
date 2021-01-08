3529202205bcab6e667dadbfad63e6fcfbe6d4df
Dan F-M <foreman.mackey@gmail.com>
2021-01-07 20:38:15 -0500

    pass env variable

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ class CMakeBuildExt(build_ext):
             "-DCMAKE_PREFIX_PATH={}".format(pybind11.get_cmake_dir()),
         ]
         if os.environ.get("KEPLER_JAX_CUDA", "no").lower() == "yes":
-            cmake_args.append("-DKEPLER_JAX_CUDA")
+            cmake_args.append("-DKEPLER_JAX_CUDA=yes")
 
         os.makedirs(self.build_temp, exist_ok=True)
         subprocess.check_call(
