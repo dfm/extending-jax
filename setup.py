79508c178b8f050ef2e246fae63ee038a17cf6af
gauenk <kent.gauen@gmail.com>
2022-09-07 20:57:07 -0400

    simple setup

--- setup.py
+++ setup.py
@@ -1,110 +1,34 @@
 #!/usr/bin/env python
 
-import codecs
-import os
-import subprocess
+import os,codecs
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-from setuptools import Extension, find_packages, setup
-from setuptools.command.build_ext import build_ext
-
+# -- read long description --
 HERE = os.path.dirname(os.path.realpath(__file__))
-
-
 def read(*parts):
     with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
         return f.read()
 
-
-# This custom class for building the extensions uses CMake to compile. You
-# don't have to use CMake for this task, but I found it to be the easiest when
-# compiling ops with GPU support since setuptools doesn't have great CUDA
-# support.
-class CMakeBuildExt(build_ext):
-    def build_extensions(self):
-        # First: configure CMake build
-        import platform
-        import sys
-        import distutils.sysconfig
-
-        import pybind11
-
-        # Work out the relevant Python paths to pass to CMake, adapted from the
-        # PyTorch build system
-        if platform.system() == "Windows":
-            cmake_python_library = "{}/libs/python{}.lib".format(
-                distutils.sysconfig.get_config_var("prefix"),
-                distutils.sysconfig.get_config_var("VERSION"),
-            )
-            if not os.path.exists(cmake_python_library):
-                cmake_python_library = "{}/libs/python{}.lib".format(
-                    sys.base_prefix,
-                    distutils.sysconfig.get_config_var("VERSION"),
-                )
-        else:
-            cmake_python_library = "{}/{}".format(
-                distutils.sysconfig.get_config_var("LIBDIR"),
-                distutils.sysconfig.get_config_var("INSTSONAME"),
-            )
-        cmake_python_include_dir = distutils.sysconfig.get_python_inc()
-
-        install_dir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath("dummy"))
-        )
-        os.makedirs(install_dir, exist_ok=True)
-        cmake_args = [
-            "-DCMAKE_INSTALL_PREFIX={}".format(install_dir),
-            "-DPython_EXECUTABLE={}".format(sys.executable),
-            "-DPython_LIBRARIES={}".format(cmake_python_library),
-            "-DPython_INCLUDE_DIRS={}".format(cmake_python_include_dir),
-            "-DCMAKE_BUILD_TYPE={}".format(
-                "Debug" if self.debug else "Release"
-            ),
-            "-DCMAKE_PREFIX_PATH={}".format(pybind11.get_cmake_dir()),
-        ]
-        if os.environ.get("KEPLER_JAX_CUDA", "no").lower() == "yes":
-            cmake_args.append("-DKEPLER_JAX_CUDA=yes")
-
-        os.makedirs(self.build_temp, exist_ok=True)
-        subprocess.check_call(
-            ["cmake", HERE] + cmake_args, cwd=self.build_temp
-        )
-
-        # Build all the extensions
-        super().build_extensions()
-
-        # Finally run install
-        subprocess.check_call(
-            ["cmake", "--build", ".", "--target", "install"],
-            cwd=self.build_temp,
-        )
-
-    def build_extension(self, ext):
-        target_name = ext.name.split(".")[-1]
-        subprocess.check_call(
-            ["cmake", "--build", ".", "--target", target_name],
-            cwd=self.build_temp,
-        )
-
-
+# -- add source files --
 extensions = [
-    Extension(
+    CUDAExtension(
         "kepler_jax.cpu_ops",
-        ["src/kepler_jax/src/cpu_ops.cc"],
+        ["lib/cpu_ops.cc"],
     ),
 ]
-
 if os.environ.get("KEPLER_JAX_CUDA", "no").lower() == "yes":
     extensions.append(
-        Extension(
+        CUDAExtension(
             "kepler_jax.gpu_ops",
             [
-                "src/kepler_jax/src/gpu_ops.cc",
-                "src/kepler_jax/src/cuda_kernels.cc.cu",
+                "lib/gpu_ops.cc",
+                "lib/kernels.cc.cu",
             ],
         )
     )
 
-
+# -- run python make --
 setup(
     name="kepler_jax",
     author="Dan Foreman-Mackey",
@@ -123,5 +47,5 @@ setup(
     install_requires=["jax", "jaxlib"],
     extras_require={"test": "pytest"},
     ext_modules=extensions,
-    cmdclass={"build_ext": CMakeBuildExt},
+    cmdclass={"build_ext": BuildExtension},
 )
